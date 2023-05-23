30aec98eccf4836e5f3e144d0b7e7acbe2f1a87c
Nicolas-Granatelli <nicknatelli@gmail.com>
2023-05-23 13:09:42 -0300

    second

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Olá tudo bem?");
+        scr.nextLine();
+
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Olá tudo bem?");
+        System.out.println("Está tudo bem sim.");
+    }
+
+}
